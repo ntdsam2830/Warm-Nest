27845a4dc080f2ec71b7b4022367dbc4606ec62d
ntdsam2830 <ntdsam@gmail.com>
2024-09-06 21:38:32 +0700

    create server

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv").config();
+const cors = require("cors");
 
 app.use(cors());
 app.use(express.json());
