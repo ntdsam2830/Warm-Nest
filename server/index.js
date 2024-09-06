21a5bbe22ee422f41365f958a17d6cf1b3728092
ntdsam2830 <ntdsam@gmail.com>
2024-09-06 13:07:21 +0700

    create index

--- server/index.js
+++ server/index.js
@@ -2,3 +2,18 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv").config();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.static("public"));
+
+const PORT = 3001;
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+  })
+  .catch((err) => console.log(`${err} did not connect`));
