41bfc547f1638433f7947a4881fdc28e37ea7be3
ntdsam2830 <ntdsam@gmail.com>
2024-09-12 12:37:07 +0700

    create login

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,17 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 
-function LoginPage() {
+const LoginPage = () => {
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+
   return (
     <div className="login">
       <div className="login_content">
         <form className="login_content_form">
-          <input type="email" placeholder="Email" required />
-          <input type="password" placeholder="Password" required />
+          <input
+            type="email"
+            placeholder="Email"
+            required
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            required
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button type="submit">LOGIN</button>
         </form>
         <a href="/register">Don't have an account? Sign in here</a>
       </div>
     </div>
   );
-}
+};
 
 export default LoginPage;
