73e401e76569aa80e66055d085939a9d93be6520
ntdsam2830 <ntdsam@gmail.com>
2024-09-20 16:58:17 +0700

    kkkk = )))

--- sam.js
+++ sam.js
@@ -13,22 +13,102 @@
 
 // console.log(scoreCount);
 
-const givenArr = [
-  { id: "123", score: "4.5" },
-  { id: "124", score: "4.5" },
-  { id: "125", score: "8" },
-  { id: "126", score: "9" },
-];
+// const scoreCount = {};
 
-const scoreCount = {};
+// for (let i = 0; i < givenArr.length; i++) {
+//   const score = givenArr[i].score;
+//   if (scoreCount[score]) {
+//     scoreCount[score] += 1;
+//   } else {
+//     scoreCount[score] = 1;
+//   }
+// }
 
-for (let i = 0; i < givenArr.length; i++) {
-  const score = givenArr[i].score;
-  if (scoreCount[score]) {
-    scoreCount[score] += 1;
-  } else {
-    scoreCount[score] = 1;
-  }
-}
+// console.log(scoreCount);
 
-console.log(scoreCount);
+// const givenArr = [0, 1, 2, 3, 4, 5, 6];
+// const doubled = givenArr.map((item) => item ** 2);
+// const even = givenArr.filter((item) => item % 2 !== 0);
+// const sum = givenArr.reduce((acc, curr) => {
+//   return acc + curr;
+// }, 0);
+// console.log(sum);
+
+// const givenFruit = ["Banana", "Orange", "Apple", "Grape"];
+// const chosenArr = givenFruit.slice(0, 4);
+// console.log(chosenArr);
+
+// const studentAs = [
+//   { name: "Alice", age: 15 },
+//   { name: "Bob", age: 16 },
+//   { name: "Lucy", age: 20 },
+//   { name: "Harry", age: 19 },
+//   { name: "Kate", age: 17 },
+//   { name: "John", age: 16 },
+//   { name: "Alice", age: 17 },
+//   { name: "Sam", age: 19 },
+//   { name: "Lily", age: 18 },
+// ];
+
+// const myArr = studentAs.splice(1, 2, { name: "Amy", age: 21 });
+// console.log(myArr);
+
+// const ageCount = studentAs.reduce((acc, curr) => {
+//   acc[curr.age] = (acc[curr.age] || 0) + 1;
+//   return acc;
+// }, {});
+
+// const ageCount = {};
+
+// for (let i = 0; i < studentAs.length; i++) {
+//   const age = studentAs[i].age;
+//   if (ageCount[age]) {
+//     ageCount[age] += 1;
+//   } else {
+//     ageCount[age] = 1;
+//   }
+// }
+
+// console.log(ageCount);
+
+// const uniqueStudents = studentAs.filter(
+//   (student, index, self) =>
+//     index === self.findIndex((s) => s.name === student.name)
+// );
+// const uniqueStudents = studentAs.reduce((acc, curr) => {
+//   const x = acc.find((student) => student.name === curr.name);
+//   if (!x) {
+//     acc.push(curr);
+//   }
+//   return acc;
+// }, []);
+
+// console.log(uniqueStudents);
+
+// const studentBs = [
+//   { name: "Alice", age: 17 },
+//   { name: "Sam", age: 19 },
+//   { name: "Lily", age: 18 },
+// ];
+
+// const aList = students.filter((student) =>
+//   student.name.toLowerCase().includes("a")
+// );
+// const sortedStudentsAs = students.sort((a, b) => a.age - b.age);
+// const totalAge = students.reduce((sum, student) => sum + student.age, 0);
+// console.log(totalAge);
+
+const user = {
+  name: "Sam",
+  age: 21,
+};
+
+const obj1 = { name: "Alice", age: 25 };
+const obj2 = { job: "Developer", city: "New York" };
+const obj3 = { hobby: "Reading", country: "USA" };
+
+// Gộp các object thành 1 object sử dụng spread operator
+const mergedObj = { ...obj1, ...obj2, ...obj3 };
+
+console.log(mergedObj);
+
---
+++ test.js
@@ -0,0 +1,4 @@
+const givenStr = "+--";
+const N = 7;
+const result = givenStr.repeat(Math.ceil(N / 3)).slice(0, N);
+console.log(result);
