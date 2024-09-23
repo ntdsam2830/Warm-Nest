a6f60e0ce6c43710dcce22e53c1e89a760a3c783
ntdsam2830 <ntdsam@gmail.com>
2024-09-23 23:33:39 +0700

    create storage

--- server/routes/listing.js
+++ server/routes/listing.js
@@ -4,3 +4,15 @@ const multer = require("multer");
 const Listing = require("../models/Listing");
 
 /* Configuration Multer for File Upload */
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "public/uploads/"); // Store uploaded files in the 'uploads' folder
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname); // Use the original file name
+  },
+});
+
+const upload = multer({ storage });
+
+
