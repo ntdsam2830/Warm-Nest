66cda2115f5d8cb224e15affe88a1da27b678cdf
ntdsam2830 <ntdsam@gmail.com>
2024-09-25 00:00:39 +0700

    add foto path to listing route

--- server/routes/listing.js
+++ server/routes/listing.js
@@ -2,6 +2,7 @@ const router = require("express").Router();
 const multer = require("multer");
 
 const Listing = require("../models/Listing");
+const User = require("../models/User");
 
 /* Configuration Multer for File Upload */
 const storage = multer.diskStorage({
@@ -15,4 +16,43 @@ const storage = multer.diskStorage({
 
 const upload = multer({ storage });
 
+/* CREATE LISTING */
+router.post("/create", upload.array("listingPhotos"), async (req, res) => {
+  try {
+    /* Take the in4 from the forms*/
+    const {
+      userId,
+      category,
+      type,
+      streetAddress,
+      aptSuite,
+      city,
+      province,
+      country,
+      bedroomCount,
+      bedCount,
+      bathroomCount,
+      amentities,
+      title,
+      description,
+      highlight,
+      highlightDesc,
+      price,
+    } = req.body;
 
+    const user = await User.findById(userId);
+
+    const listingPhotos = req.files;
+
+    if (!listingPhotos) {
+      return res.status(400).send("No file uploaded");
+    }
+
+    const listingPhotoPaths = listingPhotos.map((file) => file.path);
+
+    const newListing = new Listing({
+      userId,
+      firstName: user.firstName,
+    });
+  } catch (error) {}
+});
