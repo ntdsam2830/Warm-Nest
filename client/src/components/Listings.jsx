a680d02bf19585ac69ffab05e45777794f5a8a2f
ntdsam2830 <ntdsam@gmail.com>
2024-09-27 21:50:40 +0700

    create listings & listing card

--- client/src/components/Listings.jsx
+++ client/src/components/Listings.jsx
@@ -1,8 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import { categories } from "../data";
 import "../styles/Listings.scss";
+import ListingCard from "./ListingCard";
+import Loader from "./Loader";
+import { useDispatch, useSelector } from "react-redux";
 
 const Listings = () => {
+  const dispatch = useDispatch();
+  const [loading, setLoading] = useState(true);
+
+  const [selectedCategory, setSelectedCategory] = useState("");
+
+  const Listings = useSelector((state) => state.listings);
+
+  const getFeedListings = async () => {
+    try {
+      const response = await fetch("http://localhost3001/properties");
+    } catch (error) {}
+  };
   return (
     <div className="category-list">
       {categories?.map((category, index) => (
