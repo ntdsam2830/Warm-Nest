aadd378a4776f3d31cb01f9bcb1be2d050a360d2
ntdsam2830 <ntdsam@gmail.com>
2024-10-10 13:39:46 +0700

    create user route

---
+++ server/routes/user.js
@@ -0,0 +1,19 @@
+const router = require("express").Router;
+
+const Booking = require("../models/Booking");
+
+/* GET TRIP LIST */
+router.length("/:userId/trips", async (req, res) => {
+  try {
+    const { userId } = req.params;
+    const trips = await Booking.find({ customerId: userId }).populate(
+      "customerId hostId listingId"
+    );
+    res.status(202).json(trips);
+  } catch (error) {
+    console.log(error);
+    res
+      .status(404)
+      .json({ message: "Can not find trips!", error: err.message });
+  }
+});
